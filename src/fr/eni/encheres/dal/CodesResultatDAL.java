072c44368f1c1b039c8cc1f522e00c959bc7fdeb
Joachim44300 <joachim.desousapeixeira2020@campus-eni.fr>
2021-02-09 16:44:56 +0100

    MaJ CodesResultatDAL
    MaJ UtilisateurDAO
    
    MaJ UtilisateurDAOImpl

--- src/fr/eni/encheres/dal/CodesResultatDAL.java
+++ src/fr/eni/encheres/dal/CodesResultatDAL.java
@@ -8,20 +8,20 @@ package fr.eni.encheres.dal;
  */
 public abstract class CodesResultatDAL {
 	
-//	/**
-//	 * Echec général quand tentative d'ajouter un objet null
-//	 */
-//	public static final int INSERT_OBJET_NULL=10000;
-//	
-//	/**
-//	 * Echec général quand erreur non gérée à l'insertion 
-//	 */
-//	public static final int INSERT_OBJET_ECHEC=10001;
-//	
-//	/**
-//	 * Echec de l'insertion d'un avis à cause de la note
-//	 */
-//	public static final int INSERT_AVIS_NOTE_ECHEC=10002;
+	/**
+	 * Echec général quand tentative d'ajouter un objet null
+	 */
+	public static final int INSERT_OBJET_NULL=10000;
+	
+	/**
+	 * Echec général quand erreur non gérée à l'insertion 
+	 */
+	public static final int INSERT_OBJET_ECHEC=10001;
+	
+	/**
+	 * Echec de l'insertion d'un avis à cause de la note
+	 */
+	public static final int INSERT_AVIS_NOTE_ECHEC=10002;
 	
 	
 }
--- src/fr/eni/encheres/dal/UtilisateurDAO.java
+++ src/fr/eni/encheres/dal/UtilisateurDAO.java
@@ -1,7 +1,11 @@
 package fr.eni.encheres.dal;
 
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bo.Utilisateur;
+
 public interface UtilisateurDAO {
 
+	public void insertUtilisateur(Utilisateur utilisateur) throws BusinessException;
 	
 
 	
