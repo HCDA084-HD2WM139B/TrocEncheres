161d6145ded2cacb762b1ab3ecd41644f7bb532b
Jarousseau_Herve <jarousseau.herve@gmail.com>
2021-02-16 14:00:43 +0100

    Fonctionnalité "lister les encheres en mode déconnecté" (HJ)

--- WebContent/WEB-INF/jsp/AccueilSansConnexion.jsp
+++ WebContent/WEB-INF/jsp/AccueilSansConnexion.jsp
@@ -11,30 +11,30 @@
 	<%@include file="nav.jsp" %>
     
     <div>
-    	<h1 class="container text-center mt-4 mb-3">Liste des enchères</h1>
+    	<h1 class="container text-center mt-4 mb-3">Liste des encheres</h1>
     </div>
     
     <div class="container d-flex justify-content-between align-items-center align-self-center flex-wrap">
     	<article class="col-xl-5 col-md-12">
     		<h2>Les Filtres : </h2>
-    		<form method="POST" action"">
+    		<form method="POST" action="<%=request.getContextPath()%>/sanssession">
     			 <aside class="d-none d-lg-block d-md-none"><div class="input-group mt-1">
 				      <div class="input-group-prepend">
 				        <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
 				      </div>
-      				  <input type="text" class="form-control" placeholder="Article" aria-label="Article" aria-describedby="basic-addon1">
+      				  <input type="text" name="saisieArticle" class="form-control" placeholder="Article" aria-label="Article" aria-describedby="basic-addon1">
     			 </div></aside>
     			 
     		
     			<aside class="input-group mt-2 mb-1">
-    				<label class="col-4" for="categories">Catégories :</label>
+    				<label class="col-4" for="categories">Categories :</label>
     					<select class="col-8" name="categories" id="categories">
     						<option value"0">Toutes</option>
     						
-    						<!-- Traitement d'affichage de la liste déroulante des catégories -->
+    						<!-- Traitement d'affichage de la liste dï¿½roulante des catï¿½gories -->
     						<c:if test="${ ! empty listeCategories }">
 								<c:forEach items="${ listeCategories }" var="categorie">
-	    						<option value ="${ categorie.noCategorie }"><c:out value="${ categorie.libelle }"></c:out></option>
+	    						<option value ="${ categorie.noCategorie }">${ categorie.libelle }</option>
     							</c:forEach>
 							</c:if>
 							
@@ -60,7 +60,7 @@
     
     <div class="d-flex justify-content-between container mt-3 flex-wrap mb-5">
     
-		<!-- Traitement d'affichage des Articles dont les enchères sont en cours -->
+		<!-- Traitement d'affichage des Articles dont les enchï¿½res sont en cours -->
     	<c:if test="${ ! empty listeArticles }">
     		<c:forEach items="${ listeArticles }" var="article">
 	    		<article class="card col-xl-5 col-md-12 d-flex mt-4">
@@ -69,11 +69,11 @@
 				  		<img alt="img-default" class="img-thumbnail" src="https://zupimages.net/up/21/06/blep.png">
 				  	</div>
 				  	<div class="col-xl-7 col-md-5 d-flex flex-column">
-					    <a href="#" class="card-title badge badge-pill badge-dark">
+					    <a href="<%=request.getContextPath()%>/connexion" class="card-title badge badge-pill badge-dark">
 					    	<c:out value="${ article.nomArticle }"></c:out>
 					    </a>
 						<span>Prix : <c:out value="${ article.prixVente }"></c:out> </span>
-						<span>Fin de l'enchère : <c:out value="${ article.dateFinEchere }"></c:out> </span>
+						<span>Fin de l'enchere : <c:out value="${ article.dateFinEchere }"></c:out> </span>
 						<span class="mt-3">Vendeur : <c:out value="${ article.vendeur.pseudo }"></c:out> </span>
 				    </div>
 				  </div>
--- src/fr/eni/encheres/servlets/SansSessionServlet.java
+++ src/fr/eni/encheres/servlets/SansSessionServlet.java
@@ -17,7 +17,7 @@ import fr.eni.encheres.bo.Article;
 import fr.eni.encheres.bo.Categorie;
 
 /**
- * Servlet implementation class SansSessionServlet
+ * 
  * Servlet contrôlant l'accès aux pages non connecté (en fonction s'il y a des enchères en cours)
  * 
  */
@@ -29,40 +29,58 @@ public class SansSessionServlet extends HttpServlet {
 	// Noms des attributs des contextes de requetes
 	private static final String ATTRIBUT_LISTE_CATEGORIES = "listeCategories";
 	private static final String ATTRIBUT_LISTE_ARTICLES = "listeArticles";
+	// Noms des parametres du formulaire
+	private static final String NAME_PARAM_ARTICLE_FORMULAIRE = "saisieArticle";
+	private static final String NAME_PARAM_CATEGORIE_FORMULAIRE = "categories";
+	// Nom de la categorie par defaut : "Toutes"
+	private static final String CATEGORIE_PAR_DEFAUT = "Toutes";
 	// routes de redirections
 	private static final String JSP_ACCUEIL_SANS_CONNEXION = "/WEB-INF/jsp/AccueilSansConnexion.jsp";
 	private static final String SERVLET_DEMANDE_CONNECTION = "/connexion";
 
+	// Variable d'instance
+	private List<Article> listeArticlesComplete;
+	private List<Article> listeArticlesAfficher;
+	private List<Categorie> listeCategories;
+	
 	
 	/**
+	 * Méthode d'initialisation de la servlet.
+	 */
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		this.listeArticlesAfficher = new ArrayList<Article>();
+		this.listeArticlesComplete = new ArrayList<Article>();
+		this.listeCategories = new ArrayList<Categorie>();
+	}
+	
+	
+	/**
+	 * Méthode controlant l'affichage de la page d'accueil non connecté. 
+	 * Elle charge les articles possédant une enchère en cours. Elle redirige vers la page connexion si aucunes ressources n'est à afficher (Categories, et Articles).
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// declarations :
 		RequestDispatcher rd = null;
-		List<Categorie> listeCategories = null;
-		List<Article> listeArticles = null;
 		
 		// Appel à la BLL
 		try {
 			EnchereManager enchereManager = EnchereManager.getEnchereManager();
-
 			// Recherche des articles
-			listeArticles = enchereManager.getAllSales();
-			
+			listeArticlesComplete = enchereManager.getAllSales();
 			// Recherche des categories
 			listeCategories = enchereManager.selectionnerToutesLesCategories();
 		} catch (BusinessException be) {
 			be.printStackTrace();
 		}
 
-		if (!listeCategories.isEmpty() && !listeArticles.isEmpty() ) {
+		if (!listeCategories.isEmpty() && !listeArticlesComplete.isEmpty() ) {
 			// Dépot du résultat dans l'espace d'échange (contexte de requete)
 			request.setAttribute(ATTRIBUT_LISTE_CATEGORIES, listeCategories);
-			request.setAttribute(ATTRIBUT_LISTE_ARTICLES, listeArticles);
+			request.setAttribute(ATTRIBUT_LISTE_ARTICLES, listeArticlesComplete);
 			// Transfert de l'affichage à la JSP
 			rd = request.getRequestDispatcher(JSP_ACCUEIL_SANS_CONNEXION);
 			rd.forward(request, response);
@@ -71,16 +89,68 @@ public class SansSessionServlet extends HttpServlet {
 			rd = request.getRequestDispatcher(SERVLET_DEMANDE_CONNECTION);
 			rd.forward(request, response);
 		}
+		
 	}
 
 	
+	
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * Méthode controlant la recherche d'un article en mode déconnecté. 
+	 * Cette méthode agit sur la variable d'instance "listeArticleAfficher". 
+	 * Elle repasse le controle à la méthode doGet pour réactualiser les données.
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Declarations 
+		RequestDispatcher rd = null;
+		String saisieArticle = null;
+		String selectionCategories = null;
+		List<Article> listeArticlesFiltres = new ArrayList<Article>();
 		
+		// Récupération des parametres de filtres
+		saisieArticle = request.getParameter(NAME_PARAM_ARTICLE_FORMULAIRE);
+		selectionCategories = request.getParameter(NAME_PARAM_CATEGORIE_FORMULAIRE);
+		
+		// on re-initialise la liste à afficher car c'est une nouvelle recherche
+		listeArticlesAfficher = new ArrayList<Article>();
+		
+		// si on a une categorie en filtre on agit sur listeArticlesFiltres
+		if ( !(CATEGORIE_PAR_DEFAUT).equals(selectionCategories) ) {
+			// on parcours la liste complete pour prendre que ceux de la categorie
+			for (Article article : this.listeArticlesComplete) {
+				// si les "id categories" correspondent on l'ajoute
+				if (article.getCategorie().getnoCategorie().equals(Integer.parseInt(selectionCategories))) {
+					listeArticlesFiltres.add(article);
+				}
+			}
+		} else {
+			listeArticlesFiltres = listeArticlesComplete;
+		}
+		// si on a une saisie de recherche, on affine la recherche
+		if (!saisieArticle.isEmpty()) {
+			// on parcours la liste filtrée pour prendre que ceux qui ont le nom de l'objet
+			for (Article article : listeArticlesFiltres) {
+				// si les noms d'objet correspondent
+				if (article.getNomArticle().toLowerCase().equals(saisieArticle.toLowerCase())) {
+					listeArticlesAfficher.add(article);
+				}
+			}
+		} else {
+			listeArticlesAfficher = listeArticlesFiltres;
+		}
+		
+		// s'il y a des articles à afficher on redirige vers la jsp ; sinon on appel la méthod doGet pour réactualiser les données :
+		if (!listeCategories.isEmpty() && !listeArticlesAfficher.isEmpty() ) {
+			// Dépot du résultat dans l'espace d'échange (contexte de requete)
+			request.setAttribute(ATTRIBUT_LISTE_CATEGORIES, listeCategories);
+			request.setAttribute(ATTRIBUT_LISTE_ARTICLES, listeArticlesAfficher);
+			// Transfert de l'affichage à la JSP
+			rd = request.getRequestDispatcher(JSP_ACCUEIL_SANS_CONNEXION);
+			rd.forward(request, response);
+		} else {
+			doGet(request, response);
+		}
 	}
+	
 
 }
